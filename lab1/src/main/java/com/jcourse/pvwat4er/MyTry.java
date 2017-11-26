8d79edf397f65ee5a0fc8cd9ea0435354296ba64
pvwat4er <wat4er@gmail.com>
2017-11-26 23:32:43 +0700

    26.11.17

---
+++ lab1/src/main/java/com/jcourse/pvwat4er/MyTry.java
@@ -0,0 +1,12 @@
+package com.jcourse.pvwat4er;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import static java.nio.charset.Charset.*;
+
+public class MyTry {
+    public static void main(String[] args) {
+
+
+    }
+}
