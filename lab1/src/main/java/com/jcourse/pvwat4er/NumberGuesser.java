9a8a4cd30bf69934d286026cfdb1b87194df2c6b
pvwat4er <wat4er@gmail.com>
2017-11-26 23:50:32 +0700

    26.11.17

--- lab1/src/main/java/com/jcourse/pvwat4er/NumberGuesser.java
+++ lab1/src/main/java/com/jcourse/pvwat4er/NumberGuesser.java
@@ -1,12 +1,32 @@
 package com.jcourse.pvwat4er;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class NumberGuesser {
 
 
     public static void main(String[] args) {
-        int i = 1 + new Random().nextInt()%100;
-        System.out.println(i);
+        int i = new Random().nextInt(100) + 1;
+        // 1 + new Random().nextInt()%100;
+        int s = 0;
+        int j = 1;
+        while (j <=8){
+            Scanner scanner = new Scanner(System.in);
+            s = Integer.parseInt(scanner.nextLine());
+            if (i == s) {
+                System.out.println("Ура! Это "+ s);
+                j = 9;
+
+            }
+            else if (i >= s) {System.out.println("Загаданное число больше "+s);}
+            else  {System.out.println("Загаданное число меньше "+s);};
+            j++;
+        }
+        if (!(i == s)) {System.out.println("Загаданное число = "+i);}
+
+
+
     }
 }
+
