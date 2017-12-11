3cc61bfc95e7eaaa531c31e56b0a8a789fc65eea
Василий <33975669+pvwat4er@users.noreply.github.com>
2017-12-11 21:13:14 +0700

    Delete Command.java

--- calcfirst/src/main/java/com/jcourse/pvwat4er/Command.java
+++
@@ -1,9 +0,0 @@
-package com.jcourse.pvwat4er;
-
-import java.util.Stack;
-import java.util.Map;
-
-public interface Command {
-
-    public abstract void execute(Stack<Double> stack, Map<String, Double> define, String[] s);
-}
